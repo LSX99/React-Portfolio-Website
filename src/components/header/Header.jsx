c1c1af0bd69b72a5b53fa67ef508b724cda094be
LSX99 <shengxianglim99@gmail.com>
2023-02-02 15:20:58 +0800

    added CTA component within header file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className="container header_container">
+				<h5>Hello I'm</h5>
+				<h1>Lim Sheng Xiang</h1>
+				<h5 className="text-light">Aspiring Full Stack Developer</h5>
+				<CTA />
+			</div>
+		</header>
+	);
 };
 
 export default Header;
