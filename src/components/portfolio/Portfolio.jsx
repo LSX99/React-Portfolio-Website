e28cb791398708c1a1869f117cc93c83d35757c1
LSX99 <shengxianglim99@gmail.com>
2023-02-07 15:45:49 +0800

    Added Floating Nav jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./portfolio.css";
 
 const portfolio = () => {
-	return <div>Portfolio</div>;
+	return <section id="portfolio">Portfolio</section>;
 };
 
 export default portfolio;
