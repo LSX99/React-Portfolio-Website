ce21ba2b1245a847ec34c498059a9763845495ce
LSX99 <shengxianglim99@gmail.com>
2023-02-10 17:26:53 +0800

    added client pictures, testimonials JSX and CSS

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,58 @@
-import React from "react";
-import "./testimonials.css";
+import React from 'react';
+import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+
+const data = [
+	{
+		avatar: AVTR1,
+		name: 'Tina Snow',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum expedita accusamus exercitationem beatae impedit accusantium, itaque, excepturi, at dolores obcaecati cupiditate sequi eos facere doloremque ea quia enim suscipit laboriosam',
+	},
+	{
+		avatar: AVTR2,
+		name: 'Shatta Wale',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum expedita accusamus exercitationem beatae impedit accusantium, itaque, excepturi, at dolores obcaecati cupiditate sequi eos facere doloremque ea quia enim suscipit laboriosam',
+	},
+	{
+		avatar: AVTR3,
+		name: 'Kwame Despite',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum expedita accusamus exercitationem beatae impedit accusantium, itaque, excepturi, at dolores obcaecati cupiditate sequi eos facere doloremque ea quia enim suscipit laboriosam',
+	},
+	{
+		avatar: AVTR4,
+		name: 'Nana Ama McBrown',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum expedita accusamus exercitationem beatae impedit accusantium, itaque, excepturi, at dolores obcaecati cupiditate sequi eos facere doloremque ea quia enim suscipit laboriosam',
+	},
+];
 
 const Testimonials = () => {
-	return <section id="testimonials">Testimonials</section>;
+	return (
+		<section id='testimonials'>
+			<h5>Review from clients</h5>
+			<h2>Testimonials</h2>
+
+			<div className='container testimonials__container'>
+				{data.map(({ avatar, name, review }, index) => {
+					return (
+						<article key={index} className='testimonial'>
+							<div className='client__avatar'>
+								<img src={avatar} alt={name} />
+							</div>
+							<h5>{name}</h5>
+							<small className='client__review'>{review}</small>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Testimonials;
