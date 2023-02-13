4342fb642c44726f29e206b36d033737f4f45990
LSX99 <shengxianglim99@gmail.com>
2023-02-13 16:08:21 +0800

    customised about.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "./header.css";
-import CTA from "./CTA";
-import ME from "../../assets/me.png";
-import HeaderSocials from "./HeaderSocials";
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
 	return (
 		<header>
-			<div className="container header_container">
-				<h5>Hello I'm</h5>
-				<h1>Lim Sheng Xiang</h1>
-				<h5 className="text-light">Aspiring Full Stack Developer</h5>
+			<div className='container header_container'>
+				<h5 className='text-primary'>Hello I'm</h5>
+				<h1 className='text-primary'>Lim Sheng Xiang</h1>
+				<h5 className='text-light'>Aspiring Full Stack Developer</h5>
 				<CTA />
 				<HeaderSocials />
 
-				<div className="me">
-					<img src={ME} alt="" />
+				<div className='me'>
+					<img src={ME} alt='' />
 				</div>
 
-				<a href="#contact" className="scroll_down">
+				<a href='#contact' className='scroll_down'>
 					Scroll Down
 				</a>
 			</div>
