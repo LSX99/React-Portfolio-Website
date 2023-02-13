ff5e90721284861a8b6d22a8b62072ad44b50fae
LSX99 <shengxianglim99@gmail.com>
2023-02-13 17:01:38 +0800

    customised nav.css

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,8 +3,8 @@ import './nav.css';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
-import { RiServiceLine } from 'react-icons/ri';
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import { AiOutlineBulb } from 'react-icons/ai';
 import { useState } from 'react';
 
 const Nav = () => {
@@ -12,35 +12,35 @@ const Nav = () => {
 	return (
 		<nav>
 			<a
-				href="#"
+				href='#'
 				onClick={() => setActiveNav('#')}
 				className={activeNav === '#' ? 'active' : ''}
 			>
 				<AiOutlineHome />
 			</a>
 			<a
-				href="#about"
+				href='#about'
 				onClick={() => setActiveNav('#about')}
 				className={activeNav === '#about' ? 'active' : ''}
 			>
 				<AiOutlineUser />
 			</a>
 			<a
-				href="#experience"
+				href='#experience'
 				onClick={() => setActiveNav('#experience')}
 				className={activeNav === '#experience' ? 'active' : ''}
+			>
+				<AiOutlineBulb />
+			</a>
+			<a
+				href='#portfolio'
+				onClick={() => setActiveNav('#portfolio')}
+				className={activeNav === '#portfolio' ? 'active' : ''}
 			>
 				<BiBook />
 			</a>
 			<a
-				href="#services"
-				onClick={() => setActiveNav('#services')}
-				className={activeNav === '#services' ? 'active' : ''}
-			>
-				<RiServiceLine />
-			</a>
-			<a
-				href="#contact"
+				href='#contact'
 				onClick={() => setActiveNav('#contact')}
 				className={activeNav === '#contact' ? 'active' : ''}
 			>
